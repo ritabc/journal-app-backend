c28c24d0bab03297c1142236c5497d8889bd969b
ritabc <ritabc@gmail.com>
2020-11-27 20:11:15 -0500

    Generate user model

---
+++ db/migrate/20201128010949_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :id
+      t.string :email
+      t.string :family_name
+      t.string :given_name
+
+      t.timestamps
+    end
+  end
+end
