4e0fbffaf3f8488d88608a9094016093f55944d7
ritabc <11031915+ritabc@users.noreply.github.com>
2024-09-11 21:22:18 -0400

    Update authentication method with new, not deprecated gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,16 @@ require "securerandom"
 
 class UsersController < ApplicationController
   include SeedUserData
-  before_action :authenticator
   # In general, every action should first call authorized method (as defined by app_controller)
   # Skip it before create and login here
   skip_before_action :authorized, only: [:create, :login]
   
   def create
-    if @authenticator.valid_credentials?(user_params[:google_id_token])
-      payload = @authenticator.payload
+    puts "create"
+    payload = Google::Auth::IDTokens.verify_oidc user_params[:google_id_token], aud: Rails.application.credentials.google_client_id
+    if payload.nil?
+      render json: {error: "Invalid id token received"}
+    else
       @user = User.create(id: SecureRandom.uuid, email: payload["email"], google_account_id: payload["sub"], given_name: payload["given_name"], family_name: payload["family_name"])
       if @user.valid?
         seed_initial_user_data @user.id
@@ -18,14 +20,17 @@ class UsersController < ApplicationController
       else
         render json: {error: "Invalid username or password"}
       end
-    else 
-      render json: {error: "Invalid id token received"}
     end
   end
 
   def login
-    if @authenticator.valid_credentials?(user_params[:google_id_token])
-      payload = @authenticator.payload
+    puts user_params[:google_id_token]
+    user_params[:google_id_token]
+    payload = Google::Auth::IDTokens.verify_oidc user_params[:google_id_token], aud: Rails.application.credentials.google_client_id
+    puts payload
+    if payload.nil?
+      render json: {error: "Invalid id token received"}
+    else
       @user = User.find_by(google_account_id: payload["sub"])
       if @user 
         token = encode_token(account_id: @user.google_account_id)
@@ -33,8 +38,6 @@ class UsersController < ApplicationController
       else
         render json: {error: "User not found"}
       end
-    else
-      render json: {error: "Invalid id token received"}
     end
   end
 
@@ -47,9 +50,9 @@ class UsersController < ApplicationController
   end
 
   private
-  def authenticator
-    @authenticator ||= Authenticator.new(Rails.application.credentials.google_client_id)
-  end
+  # def authenticator
+  #   @authenticator ||= Authenticator.new(Rails.application.credentials.google_client_id)
+  # end
 
   def user_params 
     params.require(:user).permit(:google_id_token)
